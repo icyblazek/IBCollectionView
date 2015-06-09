6ed4ed00ec81d157e6952dd8b761dbe61d0195e8
Xu Lian <lianxu.keefo@gmail.com>
2015-06-09 13:48:30 -0700

    update comment

--- IBCollectionView/Source/IBCollectionSectionView.m
+++ IBCollectionView/Source/IBCollectionSectionView.m
@@ -4,7 +4,7 @@
 //
 //  Created by Kevin on 15/12/14.
 //  Copyright (c) 2014 Icyblaze. All rights reserved.
-//  https://github.com/icyblazek/IBCollectionView
+//  
 
 #import "IBCollectionSectionView.h"
 
